948739b450759654d5ad128e124cf91f9704d516
John Vandervort <jvandervort@gmail.com>
2024-03-05 18:47:51 -0800

    refactor js and extract recipe_idx module

--- js/main.js
+++ js/main.js
@@ -1,87 +1,29 @@
+import { fileList, fileIdx, getIdxJson  } from './modules/recipe_idx.js'; 
 import { filterRecipes } from './modules/search.js';
-
-// all recipes need to be added manually to FILE_LIST
-// and FILE_IDX needs to be rebuilt using URL: /?build-idx
-const FILE_LIST = [
-    'belgian_pale_ale.md'
-    ,'cristies_amber_ale.md'
-    ,'german_alt_bier.md'
-    ,'pale_ale.md'
-    ,'sierra_nevada_pale_ale.md'
-    ,'sonoran_amber_cerveza.md'  
-];
-const FILE_IDX = [
-    {
-        "file": "belgian_pale_ale.md",
-        "title": "Belgian Pale Ale",
-        "categories": [
-            "pale ale",
-            "belgian"
-        ]
-    },
-    {
-        "file": "cristies_amber_ale.md",
-        "title": "Cristie's Amber Ale",
-        "categories": [
-            "amber ale",
-            "american"
-        ]
-    },
-    {
-        "file": "german_alt_bier.md",
-        "title": "German Altbier-1",
-        "categories": [
-            "altbier",
-            "german"
-        ]
-    },
-    {
-        "file": "pale_ale.md",
-        "title": "Pale Ale",
-        "categories": [
-            "pale ale",
-            "american"
-        ]
-    },
-    {
-        "file": "sierra_nevada_pale_ale.md",
-        "title": "Sierra Nevada Pale Ale",
-        "categories": [
-            "pale ale",
-            "american"
-        ]
-    },
-    {
-        "file": "sonoran_amber_cerveza.md",
-        "title": "Sonoran Amber Cerveza",
-        "categories": [
-            "amber lager",
-            "mexican"
-        ]
-    }
-];
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     init();
 });
 
 async function init() {
-    const searchBox = document.getElementById('search-box');
-    searchBox.onkeyup = function(evt) {
-        document.getElementById("recipe-list").innerHTML = getRecipeListHtml(searchBox.value.trim());
+    document.getElementById('search-box').onkeyup = function(evt) {
+        let recipeList = filterRecipes(fileIdx, this.value.trim());
+        document.getElementById("recipe-list").innerHTML = getRecipeListHtml(recipeList);
     };
 
     const urlParams = new URLSearchParams(window.location.search);
     if (urlParams.has('build-idx')) {
-        document.body.innerHTML = await getIdx();
+        let newIdxJson = await getIdxJson();
+        document.body.innerHTML = `<pre>const fileIdx = ${newIdxJson};</pre>`;
         return;
     }
 
     const mdFilename = urlParams.get('recipe');
     const mdText = await getFullMdText(mdFilename);
     if (!mdText) {
-        const recipeList = getRecipeListHtml('');
-        document.getElementById("recipe-list").innerHTML = recipeList;
+        const recipeList = filterRecipes(fileIdx, '');
+        const recipeListHtml = getRecipeListHtml(recipeList);
+        document.getElementById("recipe-list").innerHTML = recipeListHtml;
         document.getElementById("recipe-list-container").style.display = "block";
         document.getElementById("recipe-container").style.display = "none";
         return;
@@ -93,7 +35,7 @@ async function init() {
             typographer: true
         });
     
-        const [title, mdDescription, mdIngredients, mdInstructions, mdNotes, meta] = await getMdSections(mdText);
+        const [title, mdDescription, mdIngredients, mdInstructions, mdNotes, meta] = getMdSections(mdText);
         document.title = title;
         document.getElementById("title").innerHTML = title;
         document.getElementById("description").innerHTML = md.render(mdDescription);
@@ -105,46 +47,12 @@ async function init() {
     }
 }
 
-async function getIdx() {
-    const newIdx = [];
-    for (const file of FILE_LIST) {
-        const recipe = await getFullMdText(file);
-        const [title, description, ingredients, instructions, notes, meta] = await getMdSections(recipe);
-        newIdx.push({file:file, title:title, categories:meta.categories});
-    };
-
-    return `<pre>const FILE_IDX = ${JSON.stringify(newIdx, null, 4)};</pre>`;
-}
-
-async function getFullMdText(recipeFile) {
-    if (!recipeFile) { return }
-
-    const result = await fetch(`./recipes/${recipeFile}`);
-    if (result.ok) { return result.text() }
-    
-    return null;
-}
-
-function getRecipeListHtml(searchVal) {
-    let retHtml = '';
-    let recipeList = filterRecipes(FILE_IDX, searchVal);
-    Object.keys(recipeList).sort().forEach((key) => {
-        retHtml += `<div>${capitalizeFirstLetter(key)}</div><ul>`
-        recipeList[key].forEach((element) => {
-            retHtml += `<li><a href="index.html?recipe=${element.file}">${element.title}</a></li>`;
-        });
-        retHtml += '</ul>';
-    });
-    return retHtml;
-}
-
-async function getMdSections(markdown) {
-
+function getMdSections(mdText) {
     const meta = {};
     const regexMeta = /^---[\s\S]*?(?=^---)/gm;
     const regexKeyValue = /\s*([a-z-]*):\s*(.*)/g;
     let metaList = [];
-    while ((metaList = regexMeta.exec(markdown)) !== null) {
+    while ((metaList = regexMeta.exec(mdText)) !== null) {
         let keyValList = [];
         while ((keyValList = regexKeyValue.exec(metaList[0])) !== null) {
             if (keyValList[1] === 'categories') {
@@ -158,7 +66,7 @@ async function getMdSections(markdown) {
     }
 
     const regexHeader = /(^|\n)(# (.+))([^#]+?)(## )/gm;
-    const headerExec = regexHeader.exec(markdown)
+    const headerExec = regexHeader.exec(mdText)
     const title = headerExec[3];
     let description = headerExec[4];
 
@@ -171,7 +79,7 @@ async function getMdSections(markdown) {
     let notes = '';
     const regexSubheading = /^## [\s\S]*?(?=^## |(?![\S\s]))/gm;
     let myArray;
-    while ((myArray = regexSubheading.exec(markdown)) !== null) {
+    while ((myArray = regexSubheading.exec(mdText)) !== null) {
         if (myArray[0].startsWith('## Ingredients')) {
             ingredients = myArray[0];
         }
@@ -186,6 +94,26 @@ async function getMdSections(markdown) {
     return [title, description, ingredients, instructions, notes, meta];
 }
 
+async function getFullMdText(recipeFileName) {
+    if (!recipeFileName) { return }
+    const result = await fetch(`./recipes/${recipeFileName}`);
+    if (result.ok) { return result.text() }
+    return null;
+}
+
+function getRecipeListHtml(recipeListGrouped) {
+    let retHtml = '';
+    Object.keys(recipeListGrouped).sort().forEach((key) => {
+        retHtml += `<div>${capitalizeFirstLetter(key)}</div><ul>`
+        recipeListGrouped[key].forEach((element) => {
+            retHtml += `<li><a href="index.html?recipe=${element.file}">${element.title}</a></li>`;
+        });
+        retHtml += '</ul>';
+    });
+    return retHtml;
+}
+
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
---
+++ js/modules/recipe_idx.js
@@ -0,0 +1,76 @@
+// all recipes need to be added manually to fileList
+// and fileIdx needs to be rebuilt using URL: /?build-idx
+export const fileList = [
+    'belgian_pale_ale.md'
+    ,'cristies_amber_ale.md'
+    ,'german_alt_bier.md'
+    ,'pale_ale.md'
+    ,'sierra_nevada_pale_ale.md'
+    ,'sonoran_amber_cerveza.md'  
+];
+
+export const fileIdx = [
+    {
+        "file": "belgian_pale_ale.md",
+        "title": "Belgian Pale Ale",
+        "categories": [
+            "pale ale",
+            "belgian"
+        ]
+    },
+    {
+        "file": "cristies_amber_ale.md",
+        "title": "Cristie's Amber Ale",
+        "categories": [
+            "amber ale",
+            "american"
+        ]
+    },
+    {
+        "file": "german_alt_bier.md",
+        "title": "German Altbier-1",
+        "categories": [
+            "altbier",
+            "german"
+        ]
+    },
+    {
+        "file": "pale_ale.md",
+        "title": "Pale Ale",
+        "categories": [
+            "pale ale",
+            "american"
+        ]
+    },
+    {
+        "file": "sierra_nevada_pale_ale.md",
+        "title": "Sierra Nevada Pale Ale",
+        "categories": [
+            "pale ale",
+            "american"
+        ]
+    },
+    {
+        "file": "sonoran_amber_cerveza.md",
+        "title": "Sonoran Amber Cerveza",
+        "categories": [
+            "amber lager",
+            "mexican"
+        ]
+    }
+];
+
+export async function getIdxJson() {
+    let newIdx = getIdx();
+    return JSON.stringify(newIdx, null, 4);
+}
+
+async function getIdx() {
+    const newIdx = [];
+    for (const file of fileList) {
+        const recipe = await getFullMdText(file);
+        const [title, description, ingredients, instructions, notes, meta] = await getMdSections(recipe);
+        newIdx.push({file:file, title:title, categories:meta.categories});
+    };
+    return newIdx;
+}
